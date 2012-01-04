6025c84dd0b17dcb06ba5370f4fbefecba65362c
Michael Stewart <github@thegreatmichael.com>
2012-01-04 10:27:14 -0500

    code from 2012-01-04_10-27

---
+++ division.py
@@ -0,0 +1,6 @@
+print "Integer division is like doing division and then using only the quotient with no fractions or remainders."
+print "With integer division, 3/4 = ", 3/4
+print "In order to get python to do division with a decimal or FLOATING POINT either put '.0' after the dividend or divisor.
+print "3.0/4 = ", 3.0/4
+print "you could also tell python to treat the dividend or divisor as a FLOATING POINT number."
+print "float(3)/4 = ", float(3)/4
