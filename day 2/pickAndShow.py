2f291b27e6bbd8c9821180e1ef78718f40ad67ff
Michael Stewart <github@thegreatmichael.com>
2012-01-25 08:41:29 -0500

    code from 2012-01-25_08-41

--- day 2/pickAndShow.py
+++ day 2/pickAndShow.py
@@ -1,4 +1,16 @@
-def pickAndShow():
-  f = pickAFile()
-  p = makePicture(f)
+def driver():
+  p = makePicture(pickAFile())
   show(p)
+  color = pickAColor()
+  tintAPic(p,color)
+  repaint(p)
+  
+def tintAPic(image, color):
+  for pixel in getPixels(p):
+    r,g,b = getRed(pixel), getGreen(pixel), getBlue(pixel)
+    r = (r+color.getRed())/2
+    g = (g+color.getGreen())/2
+    b = (b+color.getBlue())/2
+    pixel.setRed(r)
+    pixel.setGreen(g)
+    pixel.setBlue(b)
