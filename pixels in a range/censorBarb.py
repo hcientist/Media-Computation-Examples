7a3e4b21684485316787addc439a224060e31cb3
Michael Stewart <github@thegreatmichael.com>
2012-02-06 09:33:52 -0500

    code from 2012-02-06_09-33

--- pixels in a range/censorBarb.py
+++ pixels in a range/censorBarb.py
@@ -5,4 +5,10 @@ def censorBarb():
     for y in range(103,124):
       setColor(getPixelAt(barb, x, y), black)
   repaint(barb)
-  
+
+def lighten(pic):
+  for y in range(getHeight(pic)):
+    for x in range(getWidth(pic)):
+      pixel = getPixelAt(pic,x,y)
+      setColor(pixel,makeLighter(getColor(pixel)))
+      repaint(pic)
